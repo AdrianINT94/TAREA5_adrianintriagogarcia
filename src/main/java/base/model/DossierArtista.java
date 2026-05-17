aced1e34309b3f54a747045260a07f1c59d5cb28
ADRIAN <ADRIAN@DESKTOP-PDSC6CU>
2026-05-17 23:24:12 +0200

    Comienzo tarea 6

---
+++ src/main/java/base/model/DossierArtista.java
@@ -0,0 +1,134 @@
+package base.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Document
+public class DossierArtista {
+		
+	
+		@Id
+		private String id;
+		private String nombre;
+		private String email;
+		private String nacionalidad;
+		private String apodo;
+		private Long idArtista;
+		private List<String> especialidades;
+		private List<String> habilidades;
+		private List<Trayectoria> trayectoria;
+		private List<Observacion> observaciones;
+		private List<Evaluacion> evaluaciones;
+		
+		public DossierArtista() {}
+
+		public DossierArtista(String id, String nombre, String email, String nacionalidad, String apodo, Long idArtista,
+				List<String> especialidades, List<String> habilidades, List<Trayectoria> trayectoria,
+				List<Observacion> observaciones, List<Evaluacion> evaluaciones) {
+			super();
+			this.id = id;
+			this.nombre = nombre;
+			this.email = email;
+			this.nacionalidad = nacionalidad;
+			this.apodo = apodo;
+			this.idArtista = idArtista;
+			this.especialidades = especialidades;
+			this.habilidades = habilidades;
+			this.trayectoria = trayectoria;
+			this.observaciones = observaciones;
+			this.evaluaciones = evaluaciones;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getNacionalidad() {
+			return nacionalidad;
+		}
+
+		public void setNacionalidad(String nacionalidad) {
+			this.nacionalidad = nacionalidad;
+		}
+
+		public String getApodo() {
+			return apodo;
+		}
+
+		public void setApodo(String apodo) {
+			this.apodo = apodo;
+		}
+
+		public Long getIdArtista() {
+			return idArtista;
+		}
+
+		public void setIdArtista(Long idArtista) {
+			this.idArtista = idArtista;
+		}
+
+		public List<String> getEspecialidades() {
+			return especialidades;
+		}
+
+		public void setEspecialidades(List<String> especialidades) {
+			this.especialidades = especialidades;
+		}
+
+		public List<String> getHabilidades() {
+			return habilidades;
+		}
+
+		public void setHabilidades(List<String> habilidades) {
+			this.habilidades = habilidades;
+		}
+
+		public List<Trayectoria> getTrayectoria() {
+			return trayectoria;
+		}
+
+		public void setTrayectoria(List<Trayectoria> trayectoria) {
+			this.trayectoria = trayectoria;
+		}
+
+		public List<Observacion> getObservaciones() {
+			return observaciones;
+		}
+
+		public void setObservaciones(List<Observacion> observaciones) {
+			this.observaciones = observaciones;
+		}
+
+		public List<Evaluacion> getEvaluaciones() {
+			return evaluaciones;
+		}
+
+		public void setEvaluaciones(List<Evaluacion> evaluaciones) {
+			this.evaluaciones = evaluaciones;
+		}
+		
+}
